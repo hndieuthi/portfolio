81a31f77fe527c18123a5550bfb6da2957dd8a49
Nhan Vo Nguyen (2585) <nhanvonguyen@kms.com.vn>
2024-01-14 13:52:00 +0700

    Build angular

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-I2EQBDW4.mjs";import"./chunk-JKUJYY4D.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-KVXEXUE2.mjs";import"./chunk-JKUJYY4D.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
